434e28f00214aadb51eb62f04decb740f1fc7e9c
t4top <temitope@yahoo.co.jp>
2019-07-30 09:36:02 +0200

    Added a top header image to default page layout

--- src/.vuepress/theme/layouts/Layout.vue
+++ src/.vuepress/theme/layouts/Layout.vue
@@ -1,6 +1,10 @@
 <template>
   <LayoutWrapper>
-    <Content class="theme_content" />
+    <div class="content_wrapper">
+      <ImageFill class="topimg" src="//unsplash.it/1920/800?image=767"> <!-- overlayColor="rgba(201, 182, 167, 0.3)" -->
+      </ImageFill>
+      <Content class="theme_content" />
+    </div>
   </LayoutWrapper>
 </template>
 
@@ -15,22 +19,38 @@ export default {
 
 <style lang="stylus">
 
-.theme_content
+.content_wrapper
   position: relative
-  padding: 2rem 1.5rem
-  @media $mq_tablet
-    padding: 3rem 2rem
-  p
-    margin: 1rem 0
-    color: var(--color_on_background)
-  h1, h2, h3, h4, h5, h6
-    margin-top: 3rem
-    &:first-child
-      margin-top: 0
-    .header-anchor
-      margin-left: -.87em
-      opacity: 0
-    &:hover .header-anchor
-      opacity: 1
+  width: 100%
+  margin-top: 15rem
+  @media $mq_desktop
+    margin-top: 20rem
+  .topimg
+    background-color: var(--color_primary_variant)
+    width: 100%
+    height: 30rem
+    position: fixed
+    top: 0
+    left: 0
+  .theme_content
+    background-color: var(--color_background)
+    position: relative
+    padding: 0 1.5rem
+    @media $mq_tablet
+      padding: 0 2rem
+    p
+      margin: 1rem 0
+      color: var(--color_on_background)
+    h1, h2, h3, h4, h5, h6
+      .header-anchor
+        display: block
+        position: relative
+        margin-top: -3rem
+        padding-top: 3rem
+        margin-left: -.87em
+        opacity: 0
+      &:first-child .header-anchor
+        margin-top: 0
+        font-size: 1px
 
 </style>
