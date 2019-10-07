dcb2da14bef9796568bb71c45bfcfa97b7cb7aa3
t4top <temitope@yahoo.co.jp>
2019-10-07 16:41:08 +0200

    Added 404 page not found layout

---
+++ src/.vuepress/theme/layouts/404.vue
@@ -0,0 +1,40 @@
+<template>
+  <LayoutWrapper>
+    <div class="content_wrapper">
+      <h1>404</h1>
+      <blockquote>{{ getMsg() }}</blockquote>
+      <router-link to="/">Take me home</router-link>.
+    </div>
+  </LayoutWrapper>
+</template>
+
+<script>
+import LayoutWrapper from '@theme/components/LayoutWrapper.vue'
+
+const msgs = [
+  `We couldn't find the page you requested.`,
+  `Page not found.`,
+  `That's a Four-Oh-Four.`,
+  `This isn't the page you are looking for.`,
+  `Oops, nothing here...`
+]
+
+export default {
+  components: { LayoutWrapper },
+
+  methods: {
+    getMsg () {
+      return msgs[Math.floor(Math.random() * msgs.length)]
+    }
+  }
+}
+</script>
+
+
+<style lang="stylus">
+
+.content_wrapper
+  padding: 2rem
+  text-align: center
+
+</style>
